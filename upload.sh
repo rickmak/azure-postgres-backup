fa71fa324653585294e5ff6ca06bcb577392ec00
Rick Mak <rick.mak@gmail.com>
2020-03-27 11:30:32 +0800

    Add upload file scheme as BACKUP_NAME_SCHEME

--- upload.sh
+++ upload.sh
@@ -1,11 +1,13 @@
-#!/bin/sh -eu
+#!/bin/sh -e
+BACKUP_NAME=${BACKUP_NAME}
+
 if [ "${AZURE_STORAGE_ACCOUNT}" = "" ]; then
   echo "You need to set the AZURE_STORAGE_ACCOUNT environment variable."
   exit 1
 fi
 
 if [ "${AZURE_STORAGE_KEY}" = "" ]; then
-  echo "You need to set the $AZURE_STORAGE_KEY environment variable."
+  echo "You need to set the AZURE_STORAGE_KEY environment variable."
   exit 1
 fi
 
@@ -15,7 +17,16 @@ if [ "${AZURE_STORAGE_CONTAINER}" = "" ]; then
 fi
 
 if [ "${BACKUP_NAME}" = "" ]; then
-  BACKUP_NAME = $(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
+  BACKUP_NAME=$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
+  if [ "${BACKUP_NAME_SCHEME}" = "WEEK" ]; then
+    BACKUP_NAME=WEEKLY-$(date +"%Y-W%W").sql.gz
+  fi
+  if [ "${BACKUP_NAME_SCHEME}" = "MONTH" ]; then
+    BACKUP_NAME=MONTHLY-$(date +"%Y-M%m").sql.gz
+  fi
+  if [ "${BACKUP_NAME_SCHEME}" = "YEAR" ]; then
+    BACKUP_NAME=YEARLY-$(date +"%Y").sql.gz
+  fi
 fi
 echo "Uploading dump.sql.tgs as ${BACKUP_NAME}"
 
